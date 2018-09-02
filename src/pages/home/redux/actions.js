50de8c23cf792b0813d37e86ec5cd34ea6c5251a
shalkam <shalkamm@gmail.com>
2018-09-02 10:56:28 +0200

    changed data url

--- src/pages/home/redux/actions.js
+++ src/pages/home/redux/actions.js
@@ -5,7 +5,8 @@ export default {
     dispatch({
       type: getBucketsRequest,
       request: {
-        url: 'static/data.json',
+        url:
+          'https://raw.githubusercontent.com/shalkam/cra-rest-starter/gh-pages/static/data.json',
         method: 'GET'
       }
     })
